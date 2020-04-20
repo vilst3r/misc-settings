1f5907f022c03911b209ab07b1fab813f1905b18
vilst3r <clifford.phan@gmail.com>
2020-04-20 15:55:34 +1000

    Add script for apt

---
+++ apt_vm_setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Only works on Debian/Ubuntu
+sudo apt-get install git
+sudo apt-get install tmux
+sudo apt-get install ctags
+sudo apt-get install vim-gtk
+sudo apt-get install fzf
+sudo apt-get install silversearcher-ag
