fb1584dd580e21638da2363fdc9eac84f54a5c40
vilst3r <clifford.phan@gmail.com>
2020-06-01 15:54:11 +1000

    Add iTerm config & clean up remote dev settings

---
+++ remote-config/ubuntu_debian_vm_setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Only works on Debian/Ubuntu. Ensure you run with sudo to skip Y/N prompts
+sudo apt-get -y install git
+sudo apt-get -y install tmux
+sudo apt-get -y install ctags
+sudo apt-get -y install vim-gtk
+sudo apt-get -y install emacs
+sudo apt-get -y install fzf
+sudo apt-get -y install silversearcher-ag
+sudo apt-get -y install python3-pip
