0d88fc22e20bf713d54084abd7970dd2fa9cf3f0
vilst3r <clifford.phan@gmail.com>
2020-10-07 15:01:25 +1100

    Update remote config docs + Add yas-snippets

--- remote-config/ubuntu_debian_vm_setup.sh
+++ remote-config/ubuntu_debian_vm_setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 # Only works on Debian/Ubuntu. Ensure you run with sudo to skip Y/N prompts
+sudo apt-get -y install build-essential
+sudo apt-get -y install valgrind
 sudo apt-get -y install git
 sudo apt-get -y install tmux
 sudo apt-get -y install ctags
@@ -9,3 +11,4 @@ sudo apt-get -y install emacs
 sudo apt-get -y install fzf
 sudo apt-get -y install silversearcher-ag
 sudo apt-get -y install python3-pip
+sudo apt-get -y install npm
