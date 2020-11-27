2753563e1d57b2ec54f5fb6ba8aa0bc02bee036a
vilst3r <clifford.phan@gmail.com>
2020-11-27 17:51:59 +1100

    Add script to install LSP servers

---
+++ lsp/install_lsp_servers.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# installing LSP servers through NPM
+npm install -g pyright
+npm install -g typescript-language-server
+npm install -g vscode-html-languageserver-bin
+npm install -g vscode-css-languageserver-bin
+npm install -g vscode-json-languageserver
+npm install -g eslint
+npm install -g bash-language-server
+npm install -g vim-language-server
+npm install -g dockerfile-language-server-nodejs
+npm install -g yaml-language-server
+pip3 install cmake-language-server --user
